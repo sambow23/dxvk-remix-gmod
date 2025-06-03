022fcbf2907cb64f9713038104808517f10dbd49
CattaRappa <sambow23@gmail.com>
2025-06-03 12:55:05 -0500

    punchy defaults

--- src/dxvk/rtx_render/rtx_local_tone_mapping.cpp
+++ src/dxvk/rtx_render/rtx_local_tone_mapping.cpp
@@ -149,9 +149,9 @@ namespace dxvk {
       ImGui::Separator();
       
       // Basic controls
-      ImGui::DragFloat("AgX Gamma", &agxGammaObject(), 0.01f, -3.0f, 3.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
-      ImGui::DragFloat("AgX Saturation", &agxSaturationObject(), 0.01f, -3.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
-      ImGui::DragFloat("AgX Exposure Offset", &agxExposureOffsetObject(), 0.01f, -3.0f, 2.0f, "%.3f EV", ImGuiSliderFlags_AlwaysClamp);
+      ImGui::DragFloat("AgX Gamma", &agxGammaObject(), 0.01f, 0.0f, 3.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      ImGui::DragFloat("AgX Saturation", &agxSaturationObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      ImGui::DragFloat("AgX Exposure Offset", &agxExposureOffsetObject(), 0.01f, 0.0f, 2.0f, "%.3f EV", ImGuiSliderFlags_AlwaysClamp);
       
       ImGui::Separator();
       
