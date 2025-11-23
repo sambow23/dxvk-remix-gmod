23f72efacb5f7bf43c19222f9ab4d7fd8e2ac314
CattaRappa <sambow23@gmail.com>
2025-11-23 16:14:44 -0600

    add ignoreViewModel flag to light structures and update related methods
    we need a way to ignore lights on viewmodels

--- public/include/remix/remix.h
+++ public/include/remix/remix.h
@@ -968,7 +968,8 @@ namespace remix {
       hash = 0;
       radiance = { 1.0f, 1.0f, 1.0f };
       isDynamic = false;  // Default to static for temporal accumulation
-      static_assert(sizeof remixapi_LightInfo == 40);
+      ignoreViewModel = false;  // Default to affecting all geometry including view models
+      static_assert(sizeof remixapi_LightInfo == 48);
     }
   };
 
