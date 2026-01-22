9208c09b1cab7d492ce2514a90bfb6376258fa55
CattaRappa <sambow23@gmail.com>
2026-01-21 18:47:25 -0600

    async mod loading

--- src/dxvk/rtx_render/rtx_scene_manager.cpp
+++ src/dxvk/rtx_render/rtx_scene_manager.cpp
@@ -517,9 +517,13 @@ namespace dxvk {
 
     manageTextureVram();
 
-    if (m_enqueueDelayedClear || m_pReplacer->checkForChanges(ctx)) {
+    if (m_enqueueDelayedClear) {
       clear(ctx, true);
       m_enqueueDelayedClear = false;
+    } else if (m_pReplacer->checkForChanges(ctx)) {
+      // Async reload completed - clear caches without waitForIdle()
+      // The GPU doesn't need to be idle for us to swap data structures
+      clear(ctx, false);
     }
 
     m_cameraManager.onFrameEnd();
@@ -2641,6 +2645,10 @@ namespace dxvk {
 
   void SceneManager::clearFrameMeshHashes() {
     m_currentFrameMeshHashes.clear();
+  }
+
+  void SceneManager::enqueueClearForNextFrame() {
+    m_enqueueDelayedClear = true;
   }
 
 }  // namespace dxvk
