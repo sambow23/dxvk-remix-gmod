23c14a19a22b68bb067c50936a834caaab0f2fa5
CR <sambow23@gmail.com>
2026-02-20 11:57:34 -0600

    fix x64 input issues

--- src/dxvk/imgui/dxvk_imgui.cpp
+++ src/dxvk/imgui/dxvk_imgui.cpp
@@ -617,7 +617,7 @@ namespace dxvk {
 
     m_capture = new ImGuiCapture(this);
 
-    if (RtxOptions::useNewGuiInputMethod()) {
+    if (RtxOptions::useNewGuiInputMethod() && env::isRemixBridgeActive()) {
       m_overlayWin = new GameOverlay("RemixGuiInputSink", this);
     }
   }
@@ -941,6 +941,8 @@ namespace dxvk {
     if (showUI == UIType::None) {
       ImGui::CloseCurrentPopup();
       ImGui::GetIO().MouseDrawCursor = false;
+      // Restore OS cursor visibility (ShowCursor uses a reference counter)
+      while (ShowCursor(TRUE) < 0) { }
     } else {
       if (RtxOptions::showUICursor()) {
         ImGui::GetIO().MouseDrawCursor = true;
