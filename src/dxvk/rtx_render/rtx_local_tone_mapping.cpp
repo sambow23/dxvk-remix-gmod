NVIDIAGameWorks/dxvk-remix

dxvk: add agx tonemapper

src/dxvk/rtx_render/rtx_local_tone_mapping.cpp
@@ -133,7 +133,43 @@ namespace dxvk {
     ImGui::DragInt("Display Mip", &displayMipObject(), 0.06f, 0, 16);
     ImGui::Checkbox("Boost Local Contrast", &boostLocalContrastObject());
     ImGui::Checkbox("Use Gaussian Kernel", &useGaussianObject());
-    ImGui::Checkbox("Finalize With ACES", &finalizeWithACESObject());
+    
+    // Tone mapping operator selection
+    const char* operators[] = { "Standard", "ACES", "AgX" };
+    int currentOp = useAgX() ? 2 : (finalizeWithACES() ? 1 : 0);
+    if (ImGui::Combo("Tone Mapping Operator", &currentOp, operators, IM_ARRAYSIZE(operators))) {
+      finalizeWithACES.setDeferred(currentOp == 1);
+      useAgX.setDeferred(currentOp == 2);
+    }
+    
+    // AgX-specific controls (only show when AgX is selected)
+    if (useAgX()) {
+      ImGui::Indent();
+      ImGui::Text("AgX Controls:");
+      ImGui::Separator();
+      
+      // Basic controls
+      ImGui::DragFloat("AgX Gamma", &agxGammaObject(), 0.01f, 0.0f, 3.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      ImGui::DragFloat("AgX Saturation", &agxSaturationObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      ImGui::DragFloat("AgX Exposure Offset", &agxExposureOffsetObject(), 0.01f, 0.0f, 2.0f, "%.3f EV", ImGuiSliderFlags_AlwaysClamp);
+      
+      ImGui::Separator();
+      
+      // Look selection
+      const char* looks[] = { "None", "Punchy", "Golden", "Greyscale" };
+      ImGui::Combo("AgX Look", &agxLookObject(), looks, IM_ARRAYSIZE(looks));
+      
+      ImGui::Separator();
+      
+      // Advanced controls
+      ImGui::Text("Advanced:");
+      ImGui::DragFloat("AgX Contrast", &agxContrastObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      ImGui::DragFloat("AgX Slope", &agxSlopeObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      ImGui::DragFloat("AgX Power", &agxPowerObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      
+      ImGui::Unindent();
+    }
+    
     ImGui::DragFloat("Exposure Level", &exposureObject(), 0.01f, 0.f, 1000.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
     ImGui::DragFloat("Shadow Level", &shadowsObject(), 0.01f, -10.f, 10.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
     ImGui::DragFloat("Highlight Level", &highlightsObject(), 0.01f, -10.f, 10.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
@@ -277,7 +313,18 @@ namespace dxvk {
       pushArgs.enableAutoExposure = enableAutoExposure;
       pushArgs.performSRGBConversion = performSRGBConversion;
       pushArgs.finalizeWithACES = finalizeWithACES();
+      pushArgs.useAgX = useAgX();
       pushArgs.useLegacyACES = RtxOptions::useLegacyACES();
+      
+      // AgX parameters
+      pushArgs.agxGamma = agxGamma();
+      pushArgs.agxSaturation = agxSaturation();
+      pushArgs.agxExposureOffset = agxExposureOffset();
+      pushArgs.agxLook = agxLook();
+      pushArgs.agxContrast = agxContrast();
+      pushArgs.agxSlope = agxSlope();
+      pushArgs.agxPower = agxPower();
+      
       switch (ditherMode()) {
       case DitherMode::None: pushArgs.ditherMode = ditherModeNone; break;
       case DitherMode::Spatial: pushArgs.ditherMode = ditherModeSpatialOnly; break;

src/dxvk/rtx_render/rtx_tone_mapping.cpp
@@ -109,7 +109,42 @@ namespace dxvk {
     ImGui::Checkbox("Tonemapping Enabled", &tonemappingEnabledObject());
     if (tonemappingEnabled()) {
       ImGui::Indent();
-      ImGui::Checkbox("Finalize With ACES", &finalizeWithACESObject());
+      
+      // Tone mapping operator selection
+      const char* operators[] = { "Standard", "ACES", "AgX" };
+      int currentOp = useAgX() ? 2 : (finalizeWithACES() ? 1 : 0);
+      if (ImGui::Combo("Tone Mapping Operator", &currentOp, operators, IM_ARRAYSIZE(operators))) {
+        finalizeWithACES.setDeferred(currentOp == 1);
+        useAgX.setDeferred(currentOp == 2);
+      }
+
+      // AgX-specific controls (only show when AgX is selected)
+      if (useAgX()) {
+        ImGui::Indent();
+        ImGui::Text("AgX Controls:");
+        ImGui::Separator();
+        
+        // Basic controls
+        ImGui::DragFloat("AgX Gamma", &agxGammaObject(), 0.01f, 0.5f, 3.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        ImGui::DragFloat("AgX Saturation", &agxSaturationObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        ImGui::DragFloat("AgX Exposure Offset", &agxExposureOffsetObject(), 0.01f, -2.0f, 2.0f, "%.3f EV", ImGuiSliderFlags_AlwaysClamp);
+        
+        ImGui::Separator();
+        
+        // Look selection
+        const char* looks[] = { "None", "Punchy", "Golden", "Greyscale" };
+        ImGui::Combo("AgX Look", &agxLookObject(), looks, IM_ARRAYSIZE(looks));
+        
+        ImGui::Separator();
+        
+        // Advanced controls
+        ImGui::Text("Advanced:");
+        ImGui::DragFloat("AgX Contrast", &agxContrastObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        ImGui::DragFloat("AgX Slope", &agxSlopeObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        ImGui::DragFloat("AgX Power", &agxPowerObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        
+        ImGui::Unindent();
+      }
 
       ImGui::Combo("Dither Mode", &ditherModeObject(), "Disabled\0Spatial\0Spatial + Temporal\0");
 
@@ -254,7 +289,17 @@ namespace dxvk {
     pushArgs.colorGradingEnabled = colorGradingEnabled();
     pushArgs.enableAutoExposure = autoExposureEnabled;
     pushArgs.finalizeWithACES = finalizeWithACES();
+    pushArgs.useAgX = useAgX();
     pushArgs.useLegacyACES = RtxOptions::useLegacyACES();
+    
+    // AgX parameters
+    pushArgs.agxGamma = agxGamma();
+    pushArgs.agxSaturation = agxSaturation();
+    pushArgs.agxExposureOffset = agxExposureOffset();
+    pushArgs.agxLook = agxLook();
+    pushArgs.agxContrast = agxContrast();
+    pushArgs.agxSlope = agxSlope();
+    pushArgs.agxPower = agxPower();
 
     // Tonemap args
     pushArgs.performSRGBConversion = performSRGBConversion;

sambow23/dxvk-remix-gmod

src/dxvk/rtx_render/rtx_local_tone_mapping.cpp
@@ -133,7 +133,43 @@ namespace dxvk {
     RemixGui::DragInt("Display Mip", &displayMipObject(), 0.06f, 0, 16);
     RemixGui::Checkbox("Boost Local Contrast", &boostLocalContrastObject());
     RemixGui::Checkbox("Use Gaussian Kernel", &useGaussianObject());
-    RemixGui::Checkbox("Finalize With ACES", &finalizeWithACESObject());
+    
+    // Tone mapping operator selection
+    const char* operators[] = { "Standard", "ACES", "AgX" };
+    int currentOp = useAgX() ? 2 : (finalizeWithACES() ? 1 : 0);
+    if (RemixGui::Combo("Tone Mapping Operator", &currentOp, operators, IM_ARRAYSIZE(operators))) {
+      finalizeWithACES.setDeferred(currentOp == 1);
+      useAgX.setDeferred(currentOp == 2);
+    }
+    
+    // AgX-specific controls (only show when AgX is selected)
+    if (useAgX()) {
+      RemixGui::Indent();
+      RemixGui::Text("AgX Controls:");
+      RemixGui::Separator();
+      
+      // Basic controls
+      RemixGui::DragFloat("AgX Gamma", &agxGammaObject(), 0.01f, 0.0f, 3.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      RemixGui::DragFloat("AgX Saturation", &agxSaturationObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      RemixGui::DragFloat("AgX Exposure Offset", &agxExposureOffsetObject(), 0.01f, -2.0f, 2.0f, "%.3f EV", ImGuiSliderFlags_AlwaysClamp);
+      
+      RemixGui::Separator();
+      
+      // Look selection
+      const char* looks[] = { "None", "Punchy", "Golden", "Greyscale" };
+      RemixGui::Combo("AgX Look", &agxLookObject(), looks, IM_ARRAYSIZE(looks));
+      
+      RemixGui::Separator();
+      
+      // Advanced controls
+      RemixGui::Text("Advanced:");
+      RemixGui::DragFloat("AgX Contrast", &agxContrastObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      RemixGui::DragFloat("AgX Slope", &agxSlopeObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      RemixGui::DragFloat("AgX Power", &agxPowerObject(), 0.01f, 0.0f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+      
+      RemixGui::Unindent();
+    }
+    
     RemixGui::DragFloat("Exposure Level", &exposureObject(), 0.01f, 0.f, 1000.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
     RemixGui::DragFloat("Shadow Level", &shadowsObject(), 0.01f, -10.f, 10.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
     RemixGui::DragFloat("Highlight Level", &highlightsObject(), 0.01f, -10.f, 10.f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
@@ -277,7 +313,18 @@ namespace dxvk {
       pushArgs.enableAutoExposure = enableAutoExposure;
       pushArgs.performSRGBConversion = performSRGBConversion;
       pushArgs.finalizeWithACES = finalizeWithACES();
+      pushArgs.useAgX = useAgX();
       pushArgs.useLegacyACES = RtxOptions::useLegacyACES();
+      
+      // AgX parameters
+      pushArgs.agxGamma = agxGamma();
+      pushArgs.agxSaturation = agxSaturation();
+      pushArgs.agxExposureOffset = agxExposureOffset();
+      pushArgs.agxLook = agxLook();
+      pushArgs.agxContrast = agxContrast();
+      pushArgs.agxSlope = agxSlope();
+      pushArgs.agxPower = agxPower();
+      
       switch (ditherMode()) {
       case DitherMode::None: pushArgs.ditherMode = ditherModeNone; break;
       case DitherMode::Spatial: pushArgs.ditherMode = ditherModeSpatialOnly; break;

src/dxvk/rtx_render/rtx_tone_mapping.cpp
@@ -108,8 +108,43 @@ namespace dxvk {
 
     RemixGui::Checkbox("Tonemapping Enabled", &tonemappingEnabledObject());
     if (tonemappingEnabled()) {
-      ImGui::Indent();
-      RemixGui::Checkbox("Finalize With ACES", &finalizeWithACESObject());
+      RemixGui::Indent();
+      
+      // Tone mapping operator selection
+      const char* operators[] = { "Standard", "ACES", "AgX" };
+      int currentOp = useAgX() ? 2 : (finalizeWithACES() ? 1 : 0);
+      if (RemixGui::Combo("Tone Mapping Operator", &currentOp, operators, IM_ARRAYSIZE(operators))) {
+        finalizeWithACES.setDeferred(currentOp == 1);
+        useAgX.setDeferred(currentOp == 2);
+      }
+
+      // AgX-specific controls (only show when AgX is selected)
+      if (useAgX()) {
+        ImGui::Indent();
+        RemixGui::Text("AgX Controls:");
+        RemixGui::Separator();
+        
+        // Basic controls
+        RemixGui::DragFloat("AgX Gamma", &agxGammaObject(), 0.01f, 0.5f, 3.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        RemixGui::DragFloat("AgX Saturation", &agxSaturationObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        RemixGui::DragFloat("AgX Exposure Offset", &agxExposureOffsetObject(), 0.01f, -2.0f, 2.0f, "%.3f EV", ImGuiSliderFlags_AlwaysClamp);
+        
+        RemixGui::Separator();
+        
+        // Look selection
+        const char* looks[] = { "None", "Punchy", "Golden", "Greyscale" };
+        RemixGui::Combo("AgX Look", &agxLookObject(), looks, IM_ARRAYSIZE(looks));
+        
+        RemixGui::Separator();
+        
+        // Advanced controls
+        RemixGui::Text("Advanced:");
+        RemixGui::DragFloat("AgX Contrast", &agxContrastObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        RemixGui::DragFloat("AgX Slope", &agxSlopeObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        RemixGui::DragFloat("AgX Power", &agxPowerObject(), 0.01f, 0.5f, 2.0f, "%.3f", ImGuiSliderFlags_AlwaysClamp);
+        
+        ImGui::Unindent();
+      }
 
       RemixGui::Combo("Dither Mode", &ditherModeObject(), "Disabled\0Spatial\0Spatial + Temporal\0");
 
@@ -254,7 +289,17 @@ namespace dxvk {
     pushArgs.colorGradingEnabled = colorGradingEnabled();
     pushArgs.enableAutoExposure = autoExposureEnabled;
     pushArgs.finalizeWithACES = finalizeWithACES();
+    pushArgs.useAgX = useAgX();
     pushArgs.useLegacyACES = RtxOptions::useLegacyACES();
+    
+    // AgX parameters
+    pushArgs.agxGamma = agxGamma();
+    pushArgs.agxSaturation = agxSaturation();
+    pushArgs.agxExposureOffset = agxExposureOffset();
+    pushArgs.agxLook = agxLook();
+    pushArgs.agxContrast = agxContrast();
+    pushArgs.agxSlope = agxSlope();
+    pushArgs.agxPower = agxPower();
 
     // Tonemap args
     pushArgs.performSRGBConversion = performSRGBConversion;