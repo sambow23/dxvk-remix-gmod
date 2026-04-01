63eda6b00b30d46708bd4b81a53adadfa3de08be
CattaRappa <sambow23@gmail.com>
2026-04-01 15:43:27 -0500

    Hillaire atmosphere (backported from atmos branch)

--- src/dxvk/rtx_render/rtx_context.h
+++ src/dxvk/rtx_render/rtx_context.h
@@ -25,6 +25,7 @@
 #include "rtx_resources.h"
 #include "rtx_asset_exporter.h"
 #include "rtx_camera_manager.h"
+#include "rtx_atmosphere.h"
 #include "rtx/pass/nrd_args.h"
 
 #include <cstdint>
@@ -233,6 +234,9 @@ namespace dxvk {
     VkFormat m_skyRtColorFormat = VK_FORMAT_B10G11R11_UFLOAT_PACK32;
     VkClearValue m_skyClearValue;
     bool m_skyClearDirty = false;
+    SkyMode m_lastSkyMode = SkyMode::SkyboxRasterization;
+
+    std::unique_ptr<RtxAtmosphere> m_atmosphere;
 
     bool shouldUseDLSS() const;
     bool shouldUseRayReconstruction() const;
